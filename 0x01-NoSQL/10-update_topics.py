b37ec2eb71f698eeff3432696467d82d42852eef
Janeth Eni <enijaneth@gmail.com>
2023-04-15 06:26:40 +0100

    Update many

--- 0x01-NoSQL/10-update_topics.py
+++ 0x01-NoSQL/10-update_topics.py
@@ -2,8 +2,6 @@
 """ This script changes all topics of a school document based on the name. """
 
 
-
 def update_topics(mongo_collection, name, topics):
     """ Change school topics in a doc based on the name """
-    update_many = { "name": name }, { "$set": { "topics": topics } }
-    return mongo_collection.update_many()
+    mongo_collection.update_many({"name": name}, {"$set": {"topics": topics}})
