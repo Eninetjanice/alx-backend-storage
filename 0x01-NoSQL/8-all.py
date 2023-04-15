587aaa20437aea39a2fc67a17ca16769f60bec97
Janeth Eni <enijaneth@gmail.com>
2023-04-15 06:17:32 +0100

    List all

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -6,8 +6,9 @@ def list_all(mongo_collection):
     """ Lists all documents in a collection
     Arg: mongo_collection; pymongo collection object
     Return value: Empty list if no doc in collection """
-    search_doc = mongo_collection.find()
+    """ search_doc = mongo_collection.find()
     if search_doc.count() == 0:
         return []
     else:
-        return [doc for doc search_doc]
+        return [doc for doc search_doc]"""
+    return mongo_collection.find()
